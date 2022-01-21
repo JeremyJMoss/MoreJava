f642700633d05eaad6908785df554afbb0b50ee4
JeremyJMoss <mossyj19@gmail.com>
2022-01-21 22:59:05 +1100

    extra files

--- DiagonalStar/src/Jeremy/Moss/Main.java
+++ DiagonalStar/src/Jeremy/Moss/Main.java
@@ -3,6 +3,35 @@ package Jeremy.Moss;
 public class Main {
 
     public static void main(String[] args) {
-	
+        printSquareStar(100);
+    }
+
+    public static void printSquareStar(int number){
+        if (number < 5){
+            System.out.println("Invalid Value");
+        }
+        else {
+            for (int row = 1; row <= number; row++){
+                String stars = "";
+                for (int col = 1; col <= number; col++){
+                    if (row == 1 || row == number){
+                        stars += "*";
+                    }
+                    else if (col == 1 || col == number){
+                        stars += "*";
+                    }
+                    else if (row == col){
+                        stars += "*";
+                    }
+                    else if (col == number - row + 1){
+                        stars += "*";
+                    }
+                    else{
+                        stars += " ";
+                    }
+                }
+                System.out.println(stars);
+            }
+        }
     }
 }
---
+++ firefox/src/Jeremy/Moss/Main.java
@@ -0,0 +1,8 @@
+package Jeremy.Moss;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
